c65f9cd7b8a48b987bfddd61099304a53329827c
Luciano Amenta <lucianoamenta@gmail.com>
2024-01-01 20:23:40 -0300

    cartop full deshabilitada temporalmente

--- resources/views/partial/cards/card-cinco-automotor.blade.php
+++ resources/views/partial/cards/card-cinco-automotor.blade.php
@@ -1,23 +1,6 @@
 <div class="card card-automotor background-automotor-card-four mx-auto" style="background-color: #546e7a !important;">
-	<div class="card-body">
-        <h6 class="text-white"><span class="card-title-automotor mr-2">CAR TOP FULL</span> RESPONSABILIDAD CIVIL, PÉRDIDAS TOTALES Y PARCIALES</h6>
-		<hr class="card-automotor-separador">
-		<div class="row">
-			<div class="col-12 col-lg-9">
-				<p class="card-automotor-text">Pérdida total por accidente, robo y/o hurto e incendio total y parcial, más adicionales de granizo y cristales</p>
-				<ul class="automotor">
-					<li class="card-icon-four text-list-automotor">Responsabilidad Civil. Límite de Responsabilidad Civil:  $39.000.000</li>
-					<li class="card-icon-four text-list-automotor">Daños a personas y bienes transportados y no transportados.</li>
-					<li class="card-icon-four text-list-automotor">Daños parciales por granizo hasta la suma Asegurada.</li>
-					<li class="card-icon-four text-list-automotor">Daños a cristales y cerraduras laterales.</li>
-					<li class="card-icon-four text-list-automotor">Daños a cristales de Techo, hasta el 2% de la suma Asegurada.</li>
-					<li class="card-icon-four text-list-automotor">Daños a luneta y parabrisas, hasta la suma Asegurada.</li>
-				</ul>
-			</div>
-			<div class="col-12 col-lg-3 text-right d-flex align-items-end justify-content-end">
-				<img src="{{url('/images/incluyeserviciogrua.svg')}}" class="img-fluid">
-			</div>
-		</div>
-
-	</div>
+    <div class="card-body">
+        <!-- Imagen del card con estilos inline y URL externa -->
+        <img src="{{url('/images/cartopfull_cerrado.png')}}" style="width: 100%; height: auto; object-fit: cover;" class="img-fluid">
+    </div>
 </div>
