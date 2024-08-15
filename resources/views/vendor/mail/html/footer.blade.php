2da6a9528f2f8eb8e04c09ee867b3c846c3f545d
Pablo Godoy <pgodoyrobles@gmail.com>
2024-08-15 16:04:28 -0300

    Envio de emails footer

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -6,12 +6,12 @@
 <div style="text-align:center; "class="container-mail">
  
   	<img src="{{url('/images/mobile/logo finisterre blanco_footer.png')}}" class="imagen-mail">
-  	<a style="text-decoration: none; display:block; margin-block-start: 1em; margin-block-end: 1em; padding-top:32px;" class="enlace-mail">www.finisterre.com.ar</a>
+  	<a style="text-decoration: none; display:block; margin-block-start: 1em; margin-block-end: 1em; padding-top:32px;" class="enlace-mail">www.sistemaunicosiniestros.com</a>
 
 
   	<p style="text-align: center;" class="siniestro-footer-mail">Siniestros</p>
   	<p  style="text-align: center;"  class="interno-mail">0810 362 0700</p>
-  	<p  style="text-align: center;" class="siniestro-footer-mail">Reconquista 522 - Piso 7 - C1003ABL - C.A.B.A.</p>
+  	<p  style="text-align: center;" class="siniestro-footer-mail"></p>
 
   	<img style="text-align:center; padding-top:30px !important;" src="{{url('/images/mail/ssn horizontal.png')}}" class="imagen-mail">
 
